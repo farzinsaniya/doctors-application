2aabfe552bbb78f6cb665fefcb79e397ab8164f5
saniya <saniyafarzin75@gmail.com>
2022-03-24 15:56:34 +0530

    add sort filter

--- src/search_filter.js
+++ src/search_filter.js
@@ -3,7 +3,7 @@ import React , {useEffect, useState} from 'react'
 
 export default function SearchFilter() {
     const [data, setData] = useState([]);
-    const [searchApiData, setSearchApiData] = useState('');
+    const [searchApiData, setSearchApiData] = useState([]);
     const[filterVal, setFilterVal] = useState('');
     useEffect(() =>{
       //creating a function
@@ -43,11 +43,43 @@ export default function SearchFilter() {
 
 
       }
+      const handleSort = (e) =>{
+        //filtering dr name & hsospital name
+          if (e.target.value == '') {
+            //when no search/filtering is performed : keep the data as it is
+            setData(searchApiData);
+          }else{
+            //the data searched wil directly be converted to lowercase
+       const filterResult =  searchApiData.filter(item => item.name.toLowerCase().includes(e.target.value.toLowerCase()) || item.username.toLowerCase().includes(e.target.value.toLowerCase()) || item.email.includes(e.target.value));
+
+       if (filterResult.length>0) {
+          setData(filterResult);
+       }else{
+          setData([{"name": "No data found! Improve search results!"}])
+       }
+
+      
+          }
+          // setFilterVal(e.target.value);
+
+
+      }
   return (
     <div className='home' >
   <h1>Doctor Filtering System</h1> 
    <div className="search-field" >
      <input type={'search'} placeholder='Search here' value={filterVal} onInput={(e) => handleFilter(e) } />
+     <select onChange={handleSort} style={{width:"20vw", margin:"auto"}} className="form-select mt-3" aria-label="Default select example">
+  <option selected value=''>Fiter based on Hospital name</option>
+   {
+        searchApiData.map(item =>{
+          return(
+            <option value={item.username}>{item.username}</option>
+          )
+        })
+      }
+  
+</select>
    </div>
     <div className='container mt-4' >
     <table className='table' >
